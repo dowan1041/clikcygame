c0f7461a6362792164238128dbbdc50250daa0d4
Dowan Kim <dowankim@Dowanui-MacBook-Pro.local>
2019-06-06 21:13:08 -0400

    finished with app.js and working on some components files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -30,7 +30,18 @@ handleClick = id => {
 }
 
 handleIncrement = () => {
-
+  const newScore = this.state.score + 1;
+  this.setState({
+    score: newScore,
+    message: "You guessed correctly, you got a point."
+  });
+  if (newScore >= this.state.topScore) {
+    this.setState({
+      topScore: newScore,
+      message: "You guessed correctly, you got a high score."
+    })
+  }
+  this.randomPicture();
 }
 
 handleReset = () => {
@@ -41,6 +52,11 @@ handleReset = () => {
     clickedPic = []
   });
   this.randomPicture();
+}
+
+handleRandom = () => {
+  let randomPic = randomPicture(elements);
+  this.setState({ pictures: randomPic})
 }
 
 // clickedPictures = prop => {
@@ -65,25 +81,60 @@ handleReset = () => {
 //   }
 // }
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <Nav
+        title = "Clicky Game"
+        score = {this.state.score}
+        topScore = {this.state.topScore}
+        message = {this.state.message}
+      />
+
+      <Title>
+        Click on an image to earn points, but do not click on any more than once!
+      </Title>
+      <Container>
+        <Row>
+          {this.state.pictures.map(friend => (
+            <Column size = "md-3 sm-6">
+              <PictureCard
+                key = {picture.id}
+                handleClick = {this.handleClick}
+                handleIncrement = {this.handleIncrement}
+                handleReset = {this.handleReset}
+                handleRandom = {this.handleRandom}
+                id = {picture.id}
+                image = {picture.image}
+              />
+            </Column>
+          ))}
+        </Row>
+      </Container>
+    </Wrapper>
+  )
 }
+
 
 export default App;
