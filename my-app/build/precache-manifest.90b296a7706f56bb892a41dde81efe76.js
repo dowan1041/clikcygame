b954763a51d8fb8d730b9b5bc17bdd303a47f9e5
Dowan Kim <dowankim@Dowanui-MBP.fios-router.home>
2019-06-09 17:20:28 -0400

    heroku fix

---
+++ my-app/build/precache-manifest.90b296a7706f56bb892a41dde81efe76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a75096ea2a2a282b0d21b91066ec423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f5f4d331606f4ea570b",
+    "url": "/static/css/main.2e75292d.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3f5f4d331606f4ea570b",
+    "url": "/static/js/main.4c6be7f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
